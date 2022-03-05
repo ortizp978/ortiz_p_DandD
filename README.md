49838d1c7b1115e4eaa9c20dcf27569a9b3634e8
Pablo Ortiz <pablortiz78@MacBook-Pro-de-Pablo.local>
2022-03-05 10:45:02 -0500

    Fixing Second Bug

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ortiz_p_DandD
+## Ortiz Pablo Crushing Bugs Assignment
+
+In the following assignment the 2 pre-existing errors of the puzzle developed in class will be solved, for the first error (preventing an image from being placed on top of another) the additional pieces of code are found in the main.js file lines 63 to 69 + 109 .
+
+For the second error, lines 15 to 61 and 77 to 80 were added.
+
+### Built with [atom](https://atom.io/)
+
+### License
+MIT
+
+### Autor: Pablo Ortiz.
--- script/main.js
+++ script/main.js
@@ -8,7 +8,57 @@
 		// const is a variable whose value can't change - it's immutable. Use this to assign bits of data that will be constant (const) for the entire lifecycle of your app.
 
 		// puzzlePaths refer to half the image src that we need to build -. need to append an index to them
+
 	const puzzlePaths = ["topLeft", "topRight", "bottomLeft", "bottomRight"]
+
+
+	function CleanElements(id){
+		document.getElementById('topLeft').remove();
+		const imagen = document.createElement("img");
+		imagen.id= "topLeft";
+		imagen.draggable = true;
+		imagen.src="images/"+ imagen.id + id + ".jpg";
+		imagen.className = "puzzle-image";
+		imagen.alt= "top left";
+		imagen.addEventListener ("dragstart", dragStarted);
+		document.getElementById("divimg").appendChild(imagen);
+		}
+
+	function CleanElements1(id){
+		document.getElementById('topRight').remove();
+		const imagen = document.createElement("img");
+		imagen.id= "topRight";
+		imagen.draggable = true;
+		imagen.src="images/"+ imagen.id + id + ".jpg";
+		imagen.className = "puzzle-image";
+		imagen.alt = "top right";
+		imagen.addEventListener ("dragstart", dragStarted);
+		document.getElementById("divimg").appendChild(imagen);
+		}
+
+	function CleanElements2(id){
+		document.getElementById('bottomLeft').remove();
+		const imagen = document.createElement("img");
+		imagen.id= "bottomLeft";
+		imagen.draggable = true;
+		imagen.src="images/"+ imagen.id + id + ".jpg";
+		imagen.className = "puzzle-image";
+		imagen.alt = "bottom left";
+		imagen.addEventListener ("dragstart", dragStarted);
+		document.getElementById("divimg").appendChild(imagen);
+		}
+
+	function CleanElements3(id){
+		document.getElementById('bottomRight').remove();
+		const imagen = document.createElement("img");
+		imagen.id= "bottomRight";
+		imagen.draggable = true;
+		imagen.src="images/"+ imagen.id + id + ".jpg";
+		imagen.className = "puzzle-image";
+		imagen.alt = "bottom right";
+		imagen.addEventListener ("dragstart", dragStarted);
+		document.getElementById("divimg").appendChild(imagen);
+		}
 
 	function detectImg(id){
 
@@ -23,13 +73,21 @@
 	function changeImgSet() {
 		// The "this" keyword refers to the elemen that triggers this function (the nav button we click with the custom data attribute of bgref)
 		// debugger;
+
+		CleanElements(this.dataset.bgref);
+		CleanElements1(this.dataset.bgref);
+		CleanElements2(this.dataset.bgref);
+		CleanElements3(this.dataset.bgref);
+
+
 		gameBoard.style.backgroundImage = `url(images/background${this.dataset.bgref}.jpg)`;
 
-		// loop through all of the small draggable images and chabge their src attribute with JS
-		puzzlePaths.forEach((img, index) => {
-			puzzlePieces[index].src = `images/${img + this.dataset.bgref}.jpg`
-		});
-	}
+			// loop through all of the small draggable images and chabge their src attribute with JS
+			puzzlePaths.forEach((img, index) => {
+
+				puzzlePieces[index].src = `images/${img + this.dataset.bgref}.jpg`
+			});
+		}
 
 	function dragStarted(event) {
 		console.log('started draggin a piece');
@@ -61,10 +119,10 @@
 	}
 
 	// add event handling here -> loop through theThumbnails array and add event handling to each image
-	theThumbnails.forEach(item => item.addEventListener("click", changeImgSet));
+ 	theThumbnails.forEach(item => item.addEventListener("click", changeImgSet));
 
 	//listen for the dragstarted event on the puzzle puzzlePieces
-	puzzlePieces.forEach(piece => piece.addEventListener ("dragstart", dragStarted));
+ 	puzzlePieces.forEach(piece => piece.addEventListener ("dragstart", dragStarted));
 
 	//add event handling for the drop zones (dragover and drop)
 	dropZones.forEach(zone => {
